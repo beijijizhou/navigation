747e887839267ecaa32db0fb742314d8c20d3778
Xia Zhenchao <xiazhenchao96@gmail.com>
2024-07-18 12:17:30 -0400

    prograssive web app

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from "vite-plugin-pwa";
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),nodePolyfills()],
+  plugins: [react(),nodePolyfills(),VitePWA({ registerType: "autoUpdate" })],
   define: {
     'process.env.POLYGON_CLIPPING_MAX_QUEUE_SIZE': '1000000',
     'process.env.POLYGON_CLIPPING_MAX_SWEEPLINE_SEGMENTS': '1000000',
